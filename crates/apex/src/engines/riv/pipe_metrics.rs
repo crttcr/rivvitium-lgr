457054a989cae16413dae545c318167d9bd3a770
reid <crt@rivvit.io>
2025-06-12 20:28:06 -0500

    fmt

--- crates/apex/src/engines/riv/pipe_metrics.rs
+++ crates/apex/src/engines/riv/pipe_metrics.rs
@@ -9,9 +9,9 @@ pub enum ComponentStatus{
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct PipeMetrics{
-    pub component_id: u16,
-    pub duration_ms : u64,
-    pub byte_count  : u64,
-    pub record_count: u64,
+    pub component_id:      u16,
     pub component_status : ComponentStatus,
+    pub duration_ms :      u64,
+    pub byte_count  :      u64,
+    pub record_count:      u64,
 }
